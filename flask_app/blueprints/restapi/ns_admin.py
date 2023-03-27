b10200eaad3715e6d0a2c0450661f90a22cb4726
rafasilva111 <rafael.francisco111@gmail.com>
2023-03-27 19:36:39 +0100

    light changes to master

--- flask_app/blueprints/restapi/ns_admin.py
+++ flask_app/blueprints/restapi/ns_admin.py
@@ -11,5 +11,16 @@ admin_blueprint = Blueprint('admin_blueprint', __name__, url_prefix="/api/v1")
 
 @admin_blueprint.route("/create_tables", methods=["GET"])
 def teste():
-    DBManager().create_tables()
+    manager = DBManager()
+    manager.create_tables()
+    manager.close()
+
+    return Response(status=200, response=json.dumps("Tables successfully created."))
+
+@admin_blueprint.route("/delete_tables", methods=["GET"])
+def teste():
+    manager = DBManager()
+    manager.drop_tables()
+    manager.close()
+
     return Response(status=200, response=json.dumps("Tables successfully created."))
